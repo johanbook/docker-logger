2d98afb68c381626cfe2b353fae57d86a52cd31a
Johan Book <johan.book@desupervised.io>
2021-08-02 10:27:51 +0200

    feat: add options to log header, cookies, etc

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # docker-logger
 
-A simple server that logs incoming HTTP requests. Useful for debugging nodes in a
-Docker network.
+A simple server that logs incoming HTTP requests. Useful for debugging nodes in
+a Docker network.
 
-Runs on port 5000 by default.
+## Configurations
+
+These configurations can be given as environment variables.
+
+- `LOG_BODY`. If request body should be logged. Not enabled by default.
+- `LOG_HEADERS`. If request body should be logged. Not enabled by default.
+- `PORT`: Port server runs on. Defaults to 80.
+- `SUMMARY`: If request summary should be printed at the of request. Enabled by
+  default.
+- `VERBOSE`: If logging should be verbose. Not enabled by default.
 
 ## Example
 
--- server.js
+++ server.js
@@ -4,10 +4,39 @@ const logger = require("morgan");
 const PORT = process.env.PORT || 80;
 
 const app = express();
-app.use(logger("combined"));
-app.use((req, res) => {
-  res.send("Recieved");
+
+// Basic logger for request status, timestamp, etc
+if (process.env.SUMMARY || process.env.SUMMARY === undefined) {
+  app.use(logger("combined"));
+}
+
+function printObject(obj, prefix = "") {
+  if (!obj) return;
+  for (const [key, value] of Object.entries(obj)) {
+    console.log(`${prefix}${key}: ${value}`);
+  }
+}
+
+app.use((req, res, next) => {
+  if (process.env.VERBOSE) {
+    console.log(`\n[request]: ${req.method} ${req.originalUrl}`);
+  }
+  if (process.env.LOG_HEADERS) {
+    printObject(req.headers, "[header] ");
+  }
+
+  if (process.env.LOG_BODY) {
+    if (req.body) printObject(req.body, "[body] ");
+    else console.log("[body] no body supplied");
+  }
+
+  next();
 });
+
+app.use((req, res) => {
+  res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
   console.log(`Listening for connections on port ${PORT}`);
 });
