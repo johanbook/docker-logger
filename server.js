66dd722c67d7c7f50c61adf99e3d52776e6d4490
Johan Book <johan.book@desupervised.io>
2021-08-02 09:59:51 +0200

    feat: set port to 80

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 const app = express();
 app.use(logger("combined"));
@@ -9,5 +9,5 @@ app.use((req, res) => {
   res.send("Recieved");
 });
 app.listen(PORT, () => {
-  console.log("Listening for connections");
+  console.log(`Listening for connections on port ${PORT}`);
 });
